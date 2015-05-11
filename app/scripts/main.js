7e81a39f9240a893dfa32f985ace5c4e3ceaf0ab
Steven Speicher <stevenspeicher@gmail.com>
2015-05-11 15:52:47 -0500

    update scripting and media query for mobile first

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,15 +3,9 @@
 $(function() {
 
   	function drawGraphic() {
-
-	    // clear existing graphic
-	    // $('#chart svg').remove();
+  		
   	};
 
   	var pymChild = new pym.Child({ renderCallback: drawGraphic });
-
-  	$(window).resize(function() {
-  		drawGraphic()
-  	});
 
 });
